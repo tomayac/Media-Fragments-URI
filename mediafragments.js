01715cbeacba75c855bee2f04f28e9693f74bb6c
Thomas Steiner <steiner.thomas@gmail.com>
2011-03-22 11:26:36 +0100

    Added non-native Array.forEach handler.

--- mediafragments.js
+++ mediafragments.js
@@ -2,6 +2,26 @@ var MediaFragments = (function(window) {
   
   //  "use strict";  
   
+  if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(fun /*, thisp */) {
+      "use strict";
+      if (this === void 0 || this === null) {
+        throw new TypeError();
+      }
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (typeof fun !== "function") {
+        throw new TypeError();
+      }
+      var thisp = arguments[1];
+      for (var i = 0; i < len; i++) {
+        if (i in t) {
+          fun.call(thisp, t[i], i, t);
+        }
+      }
+    };
+  }
+  
   // '&' is the only primary separator for key-value pairs
   var SEPARATOR = '&';  
   
